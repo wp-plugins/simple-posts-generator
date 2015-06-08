9bcff5301c6a86836d6cb9d4e755d5d4c4491374
raeven <raeven@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-08 09:42:42 +0000

    Updated the author URL
    git-svn-id: https://plugins.svn.wordpress.org/simple-posts-generator/trunk@1176643 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simple-posts-generator.php
+++ simple-posts-generator.php
@@ -5,7 +5,7 @@
  * Description: A simple User and Post generator for testers and developers
  * Version: 1.3
  * Author: Earl Evan Amante
- * Author URI: https://github.com/earlamante
+ * Author URI: https://profiles.wordpress.org/raeven
  * License: GPL2
  */
 	/**
